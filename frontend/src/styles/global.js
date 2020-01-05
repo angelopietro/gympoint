d68173b9e58b802b738f2dc2a90f6584158e647d
Ângelo Pietro Rosa Rodrigues <angelopietror@gmail.com>
2020-01-05 13:15:46 -0300

    style: formatação dos estilos

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -5,26 +5,28 @@ import 'antd/dist/antd.css';
 import 'react-datepicker/dist/react-datepicker.css';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
-  * {
-    margin: 0;
-    padding: 0;
-    outline: 0;
-    box-sizing: border-box;
-    -webkit-font-smoothing: auto ;
-    -moz-osx-font-smoothing: grayscale;
-    font-size: 14px;
-    font-family: "Roboto", Arial, helvetica, sans-serif;
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
 
- }
+* {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+  -webkit-font-smoothing: auto;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 14px;
+  font-family: "Roboto", Arial, helvetica, sans-serif;
+}
 
-  *:focus {
-    outline: 0;
-  }
+*:focus {
+  outline: 0;
+}
 
-  html, body, #root {
-    height: 100%;
-    background: ${colors.gray100};
+html,
+body,
+#root {
+  height: 100%;
+  background: ${colors.gray100};
 
   a {
     text-decoration: none;
@@ -34,13 +36,15 @@ export default createGlobalStyle`
     list-style: none;
   }
 
-  input, button {
+  input,
+  button {
     border-radius: 0.4rem;
   }
 
-  input{
+  input {
     width: 100%;
     border: ${colors.gray300} 1px solid;
+
     &::placeholder {
       color: ${colors.gray400};
     }
@@ -50,92 +54,90 @@ export default createGlobalStyle`
     cursor: pointer;
   }
 
-
   h1 {
-    font-size: 2rem; /*32px*/
+    font-size: 2rem; /* 32px */
     font-weight: 600;
-    color: ${colors.gray600}
+    color: ${colors.gray600};
   }
 
   h2 {
-    font-size: 1.5rem; /*24px*/
+    font-size: 1.5rem; /* 24px */
     font-weight: 600;
-    color: ${colors.gray600}
+    color: ${colors.gray600};
   }
 
   h3 {
-    font-size: 1.17rem; /*18.72px*/
+    font-size: 1.17rem; /* 18.72px */
     font-weight: 600;
-    color: ${colors.gray600}
+    color: ${colors.gray600};
   }
 
   h4 {
-    font-size: 1rem; /*16px*/
+    font-size: 1rem; /* 16px */
     font-weight: 600;
-    color: ${colors.gray600}
+    color: ${colors.gray600};
   }
 
   h5 {
-    font-size: .87rem; /*14px*/
+    font-size: 0.87rem; /* 14px */
     font-weight: 600;
-    color: ${colors.gray600}
+    color: ${colors.gray600};
   }
 
   h6 {
-    font-size: .67rem; /*10.72px*/
+    font-size: 0.67rem; /* 10.72px */
     font-weight: 600;
-    color: ${colors.gray600}
+    color: ${colors.gray600};
   }
 
   table {
-      border-spacing: 0;
-      border-collapse: collapse;
-      width: 100%;
-      color: ${colors.gray500};
+    border-spacing: 0;
+    border-collapse: collapse;
+    width: 100%;
+    color: ${colors.gray500};
 
-      thead {
-        font-weight: bold;
-        text-transform: uppercase;
+    thead {
+      font-weight: bold;
+      text-transform: uppercase;
 
-        td {
-          padding: 0px 10px 0 0;        }
+      td {
+        padding: 0 10px 0 0;
+      }
+    }
+
+    tbody {
+
+      & tr + tr {
+        border-top: 1px solid ${colors.gray200};
       }
 
-      tbody {
+      & tr:hover {
+        border-bottom: 1px solid ${colors.gray300};
+      }
 
-        & tr + tr {
-          border-top: 1px solid ${colors.gray200};
+      td {
+        padding: 15px 10px 15px 0;
+
+        & a + a {
+          margin-left: 10px;
         }
 
-        & tr:hover {
-          border-bottom: 1px solid ${colors.gray300};
+      & div {
+          display: flex;
+          justify-content: space-between;
+
+          a {
+            margin: 0 20px;
           }
+        }
 
-        td {
-          padding: 15px 10px 15px 0;
-
-          & a + a {
-            margin-left: 10px;
-          }
-
-        & div {
-            display: flex;
-            justify-content: space-between;
-
-            a {
-              margin: 0 20px;
-            }
-          }
-
-          & :last-child {
-            display: flex;
-            justify-content: flex-end;
-            font-size: 15px;
-          }
-
+        & :last-child {
+          display: flex;
+          justify-content: flex-end;
+          font-size: 15px;
         }
       }
+    }
   }
-
 }
 `;
